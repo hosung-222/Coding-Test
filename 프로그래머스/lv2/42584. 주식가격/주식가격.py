f0ee61b8499ee65b1377196a410e75d8a5f24687
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-03-11 19:27:04 +0900

    [level 2] Title: 주식가격, Time: 18.82 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,32 +1,27 @@
 # def solution(prices):
 #     answer = [0]*len(prices)
-#     index= 0
-#     x= 0
 #     for i in range(len(prices)):
 #         cnt = 0
 #         for j in range(i+1,len(prices)):
 #             if prices[i] <= prices[j]:
 #                 cnt += 1
-#                 print (i, cnt, answer)
 #             else:
-#                 print (i, cnt,answer)
-#                 if answer[i] == 0:
-#                     cnt += 1
+#                 cnt += 1
 #                 break
 #         answer[i] = cnt
 #     return answer
 
 def solution(prices):
+    stack = []
     answer = [0] * len(prices)
     for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i] += 1
-            else:
-                answer[i] += 1
-                break
-    return answer            
-                    
-            
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+            stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
+    return answer
+
         
     
