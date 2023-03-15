3eab3466fc33ccde9ecda9eebf4c0679313fc512
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-03-15 17:53:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.68 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,7 +1,8 @@
 import re
 
 def solution(files):
-    file_parts = [re.split(r'([0-9]+)', file) for file in files]
+    file_parts = [re.split('([0-9]+)', file) for file in files]
+    print(file_parts)
     sorted_files = sorted(file_parts, key=lambda x: (x[0].lower(), int(x[1])))
     result = [''.join(file) for file in sorted_files]
     return result
