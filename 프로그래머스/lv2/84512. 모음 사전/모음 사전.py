331c8b27bf5e861c4a11853a8142fb75bb76ddd6
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-03-17 22:51:46 +0900

    [level 2] Title: 모음 사전, Time: 2.38 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,10 @@
+from itertools import product
+def solution(word):
+    words = []
+    for i in range(1,6):
+        for w in product(["A","E","I","O","U"], repeat = i):
+            words.append(''.join(list(w)))
+    words.sort()
+    
+    return words.index(word) + 1
+        
