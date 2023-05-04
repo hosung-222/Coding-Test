b6f019fe118eee0cdc2957119e9a20551f047e46
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-05-04 18:55:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int N=0;
+    
+    scanf("%d",&N);
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+           if(j<=N-i) printf(" ");
+           else printf("*");
+        }
+        printf("\n");
+    }
+}
