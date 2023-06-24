5a4799ebfe9be0b62c77602c117a4c83241a8940
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-06-24 21:39:16 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 14.63 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        
+        String front = my_string.substring(0,s);
+        String end = my_string.substring(s+overwrite_string.length());
+        front += overwrite_string + end;
+        return front;
+    }
+}
