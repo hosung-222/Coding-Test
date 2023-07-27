6acec3ee56407a93f959eadd988c19d2115a18f8
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-07-27 16:25:24 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(tickets):
+    answer = []
+    dic = {}
+    q = ["ICN"]
+    for s, e in tickets:
+        if s not in dic:
+            dic[s] = [e]
+        else:
+            dic[s].append(e)
+    
+    for k in dic.keys():
+        dic[k].sort(reverse=True)
+    
+    while q:
+        print(q)
+        top = q[-1]
+        #더 이상 갈 수 있는 도착지가 없을때
+        if (top not in dic) or (not dic[top]):
+            answer.append(q.pop())
+
+        else:
+            q.append(dic[top].pop())
+
+    return answer[::-1]
