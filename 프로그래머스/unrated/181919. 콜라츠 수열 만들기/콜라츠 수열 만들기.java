63e5cf78a8759621c4653fa1cd3c844305969963
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-10-25 23:41:13 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.05 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (n != 1){
+            arr.add(n);
+            if (n %2 == 0){
+                n /=2;
+            }else {
+                n = 3*n + 1;
+            }
+            
+        }
+        arr.add(n);
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i <arr.size() ; i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
