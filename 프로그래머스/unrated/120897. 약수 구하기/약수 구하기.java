232f0fe92f5165488024ab22750bd1b75d5b43f9
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-11-18 20:30:52 +0900

    [level unrated] Title: 약수 구하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= n ; i++) {
+            if (n % i == 0){
+                arr.add(i);
+            }
+        }
+        return arr;
+    }
+}
