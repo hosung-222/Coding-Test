a16494840541e7d6e0e570b94c8c7584738921dc
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-12-02 20:04:52 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.83 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,13 +1,9 @@
+import java.time.LocalDate;
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
-        for (int i = 0; i <3 ; i++) {
-            if (date1[i] < date2[i]){
-                return 1;
-            } else if (date1[i] > date2[i]) {
-                return 0;
-            }
-        }
-        return answer;
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        return dateA.isBefore(dateB) ? 1 : 0;
     }
 }
