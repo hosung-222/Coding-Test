cda09920f450c5c87ff483e1caf2e0c090977369
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2023-12-28 02:37:12 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -3,49 +3,45 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(maps, start_x, start_y, end_x, end_y):
+def solution(maps):
+    start_x, start_y = None, None
+    lever_x, lever_y = None, None
+    end_x, end_y = None, None
+    
+    for i in range(len(maps)):
+        if "S" in maps[i]:
+            start_x, start_y = i, maps[i].index("S")
+        if "L" in maps[i]:
+            lever_x, lever_y = i, maps[i].index("L")
+        if "E" in maps[i]:
+            end_x, end_y = i, maps[i].index("E")
+
+    to_lever = bfs(start_x, start_y, lever_x, lever_y, maps)
+    to_end = bfs(lever_x, lever_y, end_x, end_y, maps)
+    
+    if to_lever == -1 or to_end == -1:
+        return -1
+    else:
+        return to_lever + to_end
+
+def bfs(start_x, start_y, end_x, end_y, maps):
     n = len(maps)
     m = len(maps[0])
     visited = [[False] * m for _ in range(n)]
-    queue = deque()
-    queue.append((start_x, start_y, 0))
+    q = deque()
+    q.append((start_x, start_y, 0))
     visited[start_x][start_y] = True
-
-    while queue:
-        x, y, distance = queue.popleft()
-
+    
+    while q:
+        x, y, distance = q.popleft()
         if x == end_x and y == end_y:
             return distance
         
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and maps[nx][ny] != 'X':
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] != "X" and not visited[nx][ny]:
+                q.append((nx, ny, distance + 1))
                 visited[nx][ny] = True
-                queue.append((nx, ny, distance + 1))
-
+                
     return -1
-
-def solution(maps):
-    lever_x, lever_y = None, None
-    start_x, start_y = None, None
-    exit_x, exit_y = None, None
     
-    n = len(maps)
-    m = len(maps[0])
-
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 'L':
-                lever_x, lever_y = i, j
-            elif maps[i][j] == 'S':
-                start_x, start_y = i, j
-            elif maps[i][j] == 'E':
-                exit_x, exit_y = i, j
-    
-
-    lever_distance = bfs(maps, start_x, start_y, lever_x, lever_y)
-    exit_distance = bfs(maps, lever_x, lever_y, exit_x, exit_y)
-    
-    if lever_distance == -1 or exit_distance == -1:
-        return -1
-    return lever_distance + exit_distance
