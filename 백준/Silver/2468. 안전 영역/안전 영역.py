74aa482d9dbd6d339de5a68ad0c070c5781bc249
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-08-14 22:17:35 +0900

    [Silver I] Title: 안전 영역, Time: 1516 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,18 +1,16 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 n = int(input())
+max_height = 0
 land = [list(map(int, input().split())) for _ in range(n)]
-
-# 지형에서의 최대 높이를 찾습니다.
 max_height = max(max(row) for row in land)
 
 result = []
-for t in range(max_height + 1):  # 강수량을 0부터 max_height까지로 설정
+for t in range(max_height + 1):
     temp = 0
     visited = [[False] * n for _ in range(n)]  
 
@@ -20,11 +18,11 @@ for t in range(max_height + 1):  # 강수량을 0부터 max_height까지로 설�
         for j in range(n):
             if land[i][j] > t and not visited[i][j]:
                 visited[i][j] = True
-                temp += 1
+                temp +=1
                 q = deque()
                 q.append((i,j))
                 while q:
-                    x, y = q.popleft()
+                    x,y = q.popleft()
                     for k in range(4):
                         nx = x + dx[k]
                         ny = y + dy[k]
@@ -34,5 +32,6 @@ for t in range(max_height + 1):  # 강수량을 0부터 max_height까지로 설�
                             visited[nx][ny] = True
                             q.append((nx,ny))
     result.append(temp)
-
+if(max(result)<0):
+    print(1)
 print(max(result))
