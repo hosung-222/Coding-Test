fb826768f1a0b43e8342d6350886f9c6771e9cb4
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-08-14 22:15:13 +0900

    [Silver I] Title: 안전 영역, Time: 1644 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+n = int(input())
+land = [list(map(int, input().split())) for _ in range(n)]
+
+# 지형에서의 최대 높이를 찾습니다.
+max_height = max(max(row) for row in land)
+
+result = []
+for t in range(max_height + 1):  # 강수량을 0부터 max_height까지로 설정
+    temp = 0
+    visited = [[False] * n for _ in range(n)]  
+
+    for i in range(n):
+        for j in range(n):
+            if land[i][j] > t and not visited[i][j]:
+                visited[i][j] = True
+                temp += 1
+                q = deque()
+                q.append((i,j))
+                while q:
+                    x, y = q.popleft()
+                    for k in range(4):
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                            continue
+                        if land[nx][ny] > t and not visited[nx][ny]:
+                            visited[nx][ny] = True
+                            q.append((nx,ny))
+    result.append(temp)
+
+print(max(result))
