405b00e8c354e214b5e604ed753ae6b138c9d5d5
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-08-29 21:57:05 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = [i for i in range(n+1)]
+dp = [[1] * (n+1) for _ in range(k+1)]
+
+for i in range(2, k+1):
+    for j in range(1, n+1):
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000
+print(dp[k][n])
