2fd1a28ea4581a446596ef9b5454287d7d175f61
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-10-28 01:08:46 +0900

    [level 1] Title: 소수 찾기, Time: 28.95 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,22 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        // 에라토스테네스의 체 알고리즘 
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        return (int) IntStream.rangeClosed(2,n)
+            .filter(i -> isPrime[i])
+            .count();
+    }
+}
