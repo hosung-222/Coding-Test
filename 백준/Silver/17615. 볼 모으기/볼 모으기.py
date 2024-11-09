ffdf235fe2687c7055baddf8d24ef35d7842085e
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-11-09 19:31:46 +0900

    [Silver I] Title: 볼 모으기, Time: 360 ms, Memory: 42840 KB, Score: 29 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -4,22 +4,42 @@ balls = list(input().strip())
 red_balls = balls.copy()
 blue_balls = balls.copy()
 
-blue_count = 0
-red_count = 0
-b_find = False
-r_find = False
+result = int(1e9)
+find = False
+count = 0
 for i in range(len(red_balls)-1, -1, -1):
     if red_balls[i] == "B":
-        b_find = True
+        find = True
     
-    if red_balls[i] == "R" and b_find == True:
-        red_count += 1
-
+    if red_balls[i] == "R" and find == True:
+        count += 1
+    
+result = min(result, count)
+count = 0
+find = False
+for i in range(len(red_balls)):
+    if red_balls[i] == "B":
+        find = True
+    
+    if red_balls[i] == "R" and find == True:
+        count += 1
+result = min(result, count)
+count = 0
+find = False
 for i in range(len(blue_balls)-1, -1, -1):
     if blue_balls[i] == "R":
-        r_find = True
+        find = True
     
-    if blue_balls[i] == "B" and r_find == True:
-        blue_count += 1
-
-print(min(blue_count, red_count))
+    if blue_balls[i] == "B" and find == True:
+        count += 1
+result = min(result, count)
+count = 0
+b_find = False
+for i in range(len(blue_balls)):
+    if blue_balls[i] == "R":
+        find = True
+    
+    if blue_balls[i] == "B" and find == True:
+        count += 1
+result = min(result, count)
+print(min(result, count))
